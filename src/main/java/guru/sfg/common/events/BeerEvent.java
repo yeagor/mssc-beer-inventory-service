968623397f175afc07dc7d3777d6a23085b9e5b9
Georgy Vashchenko <gvashchenko@lineate.com>
2020-09-05 21:15:49 +0600

    Add NewInventoryListener
    Disable bootstrap inventory creation

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,17 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class BeerEvent {
+
+    static final long serialVersionUID = -3398409198737830544L;
+
+    private BeerDto beerDto;
+}
